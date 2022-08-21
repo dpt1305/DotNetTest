5a0e4a8658ee815c46cec544720b449257f65340
tungjj <tungplatin@gmail.com>
2022-08-21 19:54:36 +0700

    init service

---
+++ HumanResources2/Service/EmployeeService.cs
@@ -0,0 +1,14 @@
+﻿using HumanResources2.Repository;
+
+namespace HumanResources2.Service
+{
+    public class EmployeeService
+    {
+        private readonly EmployeeRepository _employeeRepository;
+        public EmployeeService(EmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+    }
+}
